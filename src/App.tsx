954abce7da44108892ab0666d38382031a572daa
GuiEstevamCorrea <guilhermecorrea.2002@hotmail.com>
2023-08-08 09:20:22 -0300

    Parte Final completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { connect } from 'react-redux'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { increment, incrementAmount } from "./features/counter/counter-slice";
+import { useAppSelector } from "./hook";
 
-function App({count, increment, incrementAmount}) {
+function App() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   function handleOnClick() {
-    increment()
+    // increment()
+    // dispatch({ type: "counter/increment" });
+    dispatch(increment());
   }
 
   function handleOnClickAmount() {
-    incrementAmount(5)
+    // incrementAmount(5)
+    // dispatch({ type: "counter/incrementAmount", payload: 5 });
+    dispatch(incrementAmount(5));
   }
 
   return (
@@ -26,12 +33,8 @@ function App({count, increment, incrementAmount}) {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={handleOnClick}>
-          count is {count}
-        </button>
-        <button onClick={handleOnClickAmount}>
-          Increment Amount {count}
-        </button>
+        <button onClick={handleOnClick}>count is {count}</button>
+        <button onClick={handleOnClickAmount}>Increment Amount {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -40,20 +43,25 @@ function App({count, increment, incrementAmount}) {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-const mapStateToProps = (state) => {
+export default App;
+
+// OLD SCHOOL
+/* const mapStateToProps = (state) => {
   return {
     count: state.counter.value,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-  return{
-    increment: () => dispatch({type: "counter/increment"}),
-    incrementAmount: (amount) => dispatch({type: "counter/incrementAmount", payload: amount })
-  }
-}
+  return {
+    increment: () => dispatch({ type: "counter/increment" }),
+    incrementAmount: (amount) =>
+      dispatch({ type: "counter/incrementAmount", payload: amount }),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+ */
